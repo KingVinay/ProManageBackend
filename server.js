789554645b208c22c9881f8e9dd56920e0d6232b
Vinay Singh <vinaysthakur15@gmail.com>
2024-06-18 19:54:52 +0530

    Started server at port and required libraries installed with express parser and cors

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
+const cors = require("cors");
+app.use(cors());
+
+// express parser
+app.use(express.json());
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => console.log("DB Failed to Connect!", err));
+
 app.get("/api/health", (req, res) => {
   console.log("hey health!");
   res.json({
